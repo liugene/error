0eabef6e8705dfec7e9c8d5a64b703b6c7979bf0
liugene <750688237@qq.com>
2018-03-11 23:42:29 +0800

    init

---
+++ src/exception/ContainerException.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace linkphp\error\exception;
+
+class ContainerException extends \Exception{}
---
+++ src/exception/Handle.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace linkphp\error\exception;
+
+use linkphp\error\Error;
+
+class Handle
+{
+
+    public function handle(Error $error)
+    {
+        $data = [
+            'file'    => $error->getFile(),
+            'line'    => $error->getLine(),
+            'error_type' => $error->getErrorType(),
+            'message'   => $error->getMessage(),
+            'datetime'  => $error->getTimestamp(),
+            'trace'    => $error->getTrace()
+        ];
+        extract($data);
+        include($error->getErrorView());
+    }
+
+}
